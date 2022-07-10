03da7dd2fc2767b1372ce515cfa28cefa22a721e
Christiane Hertwig <christiane.hertwig@googlemail.com>
2022-07-10 23:42:48 +0200

    start singlepageapp

---
+++ main.js
@@ -0,0 +1,11 @@
+export default function SinglePage() {}
+
+//missing - const homepage
+const bookmarkpage = document.querySelector('[data-js="page_bookmarks"]');
+const createpage = document.querySelector('[data-js="page_create"]');
+const profilepage = document.querySelector('[data-js="page_profile"]');
+
+const navhome = document.querySelector('[data-js="nav-home"]');
+const navbookmark = document.querySelector('[data-js="nav-bookmark"]');
+const navcreate = document.querySelector("[data-js=nav-create");
+const navprofile = document.querySelector('[data-js="nav-profile');
